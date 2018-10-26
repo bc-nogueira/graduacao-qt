74fb0ce3ef2994228630ff29541108d26d9d1f73
Breno Nogueira <bcnogueira@id.uff.br>
2018-10-26 17:18:55 -0300

    Removendo diretório de testes não utilizado.

--- test/controllers/alunos_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AlunosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get alunos_index_url
-    assert_response :success
-  end
-
-end
