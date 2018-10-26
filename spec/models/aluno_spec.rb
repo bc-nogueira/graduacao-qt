45531eb1d160cf66bc78c44d32b22307474fc449
Breno Nogueira <bcnogueira@id.uff.br>
2018-10-26 17:03:30 -0300

    Adionando testes de modelo.

--- spec/models/aluno_spec.rb
+++ spec/models/aluno_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Aluno, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Methods' do
+    describe '#calcula_cr' do
+      subject { aluno.calcula_cr }
+
+      let(:aluno) { FactoryBot.build :aluno }
+
+      let!(:historico1) do
+        FactoryBot.create :hist_aprovado, nota: 8, aluno: aluno
+      end
+      let!(:historico2) do
+        FactoryBot.create :hist_aprovado, nota: 6, aluno: aluno
+      end
+
+      it { is_expected.to eq 7.0 }
+    end
+  end
 end
--- spec/models/historico_spec.rb
+++ spec/models/historico_spec.rb
@@ -1,12 +1,93 @@
 require 'rails_helper'
 
 RSpec.describe Historico, type: :model do
-  let(:historico) { FactoryBot.build :hist_aprovado }
   describe 'Methods' do
     describe '#aprovado?' do
       subject { historico.aprovado? }
 
-      it { is_expected.to be_truthy }
+      context 'Quando histórico for aprovado direto' do
+        let(:historico) { FactoryBot.build :hist_aprovado }
+        
+        it 'Deve retornar true' do
+          is_expected.to be_truthy
+        end
+      end
+
+      context 'Quando histórico for aprovado na VS' do
+        let(:historico) { FactoryBot.build :hist_aprov_vs }
+        
+        it 'Deve retornar true' do
+          is_expected.to be_truthy
+        end
+      end
+
+      context 'Quando histórico for reprovado por frequência' do
+        let(:historico) { FactoryBot.build :hist_reprov_freq }
+
+        it 'Deve retornar false' do
+          is_expected.to be_falsey
+        end
+      end
+
+      context 'Quando histórico for reprovado direto' do
+        let(:historico) { FactoryBot.build :hist_reprov }
+
+        it 'Deve retornar true' do
+          is_expected.to be_falsey
+        end
+      end
+
+      context 'Quando histórico for reprovado na VS' do
+        let(:historico) { FactoryBot.build :hist_reprov_vs }
+
+        it 'Deve retornar true' do
+          is_expected.to be_falsey
+        end
+      end
+    end
+
+    describe '#nota_final' do
+      subject { historico.nota_final }
+
+      context 'Quando histórico tiver nota maior que 6.0' do
+        let(:historico) { FactoryBot.build :historico, nota: 6.5 }
+
+        it 'Deve ter nota final igual a nota' do
+          is_expected.to eq 6.5
+        end
+      end
+
+      context 'Quando histórico tiver nota menor que 4.0' do
+        let(:historico) { FactoryBot.build :historico, nota: 3.0 }
+
+        it 'Deve ter nota final igual a nota' do
+          is_expected.to eq 3.0
+        end
+      end
+
+      context 'Quando histórico tiver nota entre 4 e 6 e vs maior que 6' do
+        let(:historico) { FactoryBot.build :historico, nota: 5.0, notavs: 7 }
+
+        it 'Deve ter nota final igual a nota VS' do
+          is_expected.to eq 7.0
+        end
+      end
+
+      context 'Quando histórico tiver nota entre 4 e 6 e vs menor que 6' do
+        let(:historico) { FactoryBot.build :historico, nota: 5.0, notavs: 3.0 }
+
+        it 'Deve ter nota final igual a média entre nota e nota VS' do
+          is_expected.to eq 4.0
+        end
+      end
+
+      context 'Quando histórico tiver nota entre 4 e 6 e não fizer vs' do
+        let(:historico) { FactoryBot.build :historico, nota: 5.0 }
+
+        it 'Deve retornar true' do
+          is_expected.to eq 2.5
+        end
+      end
     end
   end
 end
