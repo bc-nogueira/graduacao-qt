46beb4b4f46c848f7a68963e91a27ad6a2145599
Breno Nogueira <bcnogueira@id.uff.br>
2018-10-29 16:15:59 -0300

    Testes para controlador de aluno.

--- spec/controllers/alunos_controller_spec.rb
+++ spec/controllers/alunos_controller_spec.rb
@@ -1,11 +1,20 @@
 RSpec.describe AlunosController, type: :controller do
   describe 'GET #index' do
-    context 'Quando não tem aluno selecionado' do
-      before(:each) { get :index }
+    before(:each) { get :index }
 
-      it { expect(response.status).to eq 200 }
-      it { is_expected.to render_template :index }
-      it { expect(assigns(:alunos)).to be_a ActiveRecord::Relation }
-    end
+    it { expect(response.status).to eq 200 }
+    it { is_expected.to render_template :index }
+    it { expect(assigns(:alunos)).to be_a ActiveRecord::Relation }
+  end
+
+  describe 'GET #show' do
+    let(:aluno) { FactoryBot.create :aluno }
+
+    before(:each) { get :show, params: { id: aluno.id } }
+
+
+    it { expect(response.status).to eq 200 }
+    it { is_expected.to render_template :show }
+    it { expect(assigns(:aluno)).to eq aluno }
   end
 end
